913419717643d17f1f8a74b40b5e793788d50af0
Marco Ferrati <marco.ferrati@gmail.com>
2024-11-14 09:47:05 +0100

    Improved Place.kt features

--- src/main/kotlin/place/Place.kt
+++ src/main/kotlin/place/Place.kt
@@ -9,10 +9,13 @@ class Place {
     fun add(token: Token) = tokens.add(token)
     fun add(tokens: List<Token>) = this.tokens.addAll(tokens)
 
-    fun take(index: Int): Token = tokens.removeAt(index)
-
-    fun take(id: List<UUID>): Token{
-        val tokenIndex = tokens.indexOfFirst { it.ids == id }
-        return take(tokenIndex)
+    fun take(id: List<UUID>): Token = tokens.removeAt(tokens.indexOfFirst { it.ids == id })
+    fun take(): Token = tokens.removeAt(0)
+    fun take(n: Int): List<Token> {
+        require(tokens.count() >= n) { "There are not enough tokens in place (request ${n}, found ${count()})" }
+        return (0 until n).map { tokens.removeAt(0) }
     }
+
+    fun count(): Int = tokens.count()
+    fun isNotEmpty(): Boolean = tokens.isNotEmpty()
 }
