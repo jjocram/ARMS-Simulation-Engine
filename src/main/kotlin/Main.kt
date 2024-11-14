5492c556d08ff46c0ccf9c922042c54becae17a9
Marco Ferrati <marco.ferrati@gmail.com>
2024-11-14 09:49:04 +0100

    Update main to work with Process.kt

--- src/main/kotlin/Main.kt
+++ src/main/kotlin/Main.kt
@@ -1,8 +1,5 @@
-import element.*
 import org.kalasim.*
-import place.Place
 import java.io.File
-import kotlin.random.Random
 import kotlin.system.exitProcess
 
 fun main(args: Array<String>) {
@@ -14,33 +11,14 @@ fun main(args: Array<String>) {
 
     createSimulation {
         enableComponentLogger()
-        val p1 = Place()
-        val p2 = Place()
-        val p3 = Place()
-        val p4 = Place()
-        val p5 = Place()
-        val p6 = Place()
-        val p7 = Place()
-        val p1Product = Place()
-        val p2Product = Place()
-        val p3Product = Place()
-        val p4Product = Place()
-        val p5Product = Place()
-        val p6Product = Place()
-        val p7Product = Place()
-
-        StartEvent("1", "Start", p1, p1Product, 10)
-        ExclusiveSplitGateway("2", "splitCheck", p1, p1Product, listOf(
-            ExclusiveSplitCondition(p2, p2Product, false, { return@ExclusiveSplitCondition Random.nextDouble() >= 0.2 }),
-            ExclusiveSplitCondition(p4, p4Product, true, { return@ExclusiveSplitCondition false }),))
-        Activity("3", "A", p2, p3, p2Product, p3Product)
-        Activity("4", "B", p4, p5, p4Product, p5Product)
-        ExclusiveJoinGateway("5", "joinCheck", listOf(p3, p5), p6, listOf(p3Product, p5Product), p6Product)
-        EndEvent("6", "End", p6, p7, p6Product, p7Product)
+        val process = Process(processFile)
+        process.executors
+        process.bpmnElements
 
         object : Component("Watcher") {
             override fun repeatedProcess(): Sequence<Component> = sequence {
-                if (p7.tokens.count() == 10 && p7Product.tokens.count() == 10) {
+                if (process.places.getValue("end").count() == 10 && process.places.getValue("end_product").count() == 10) {
+                    println("There are 10 token in the last place")
                     stopSimulation()
                 }
 
