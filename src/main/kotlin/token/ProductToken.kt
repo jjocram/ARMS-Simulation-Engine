191ad1729d7c67e559eec7a1baf111aa3464efd6
Marco Ferrati <marco.ferrati@gmail.com>
2024-11-14 09:45:30 +0100

    Update tokens

--- src/main/kotlin/token/ProductToken.kt
+++ src/main/kotlin/token/ProductToken.kt
@@ -1,20 +1,18 @@
 package token
 
 class ProductToken: Token() {
-    private val productProperties: MutableMap<String, String> = mutableMapOf()
-
-    fun addProperty(name: String, value: String) {
-        require(name !in productProperties) {"${name} is already defined in ProductToken"}
-        productProperties[name] = value
-    }
+    val productProperties: MutableMap<String, String> = mutableMapOf()
 
     fun setProperty(name: String, value: String) {
-        require(name in productProperties) {"${name} is not present in ProductToken"}
-        productProperties[name] = value
+        productProperties.set(name, value)
     }
 
     fun getProperty(name: String): String {
         require(name in productProperties) {"${name} is not present in ProductToken"}
         return productProperties.getValue(name)
+    }
+
+    fun apply(transformation: Map<String, String>) {
+        transformation.forEach { setProperty(it.key, it.value) }
     }
 }
---
+++ src/main/kotlin/token/ResourceToken.kt
@@ -0,0 +1,4 @@
+package token
+
+class ResourceToken: Token() {
+}
