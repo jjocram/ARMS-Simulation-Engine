ca0ccb6e5a663da0283241c9f810213e551d2ab0
Marco Ferrati <marco.ferrati@gmail.com>
2024-11-14 09:45:02 +0100

    Improved Transition.kt

--- src/main/kotlin/transition/Transition.kt
+++ src/main/kotlin/transition/Transition.kt
@@ -6,14 +6,19 @@ import java.util.UUID
 class Transition(val name: String, val canFirePredicate: (Transition) -> List<UUID>?) {
     val places = mutableMapOf<String, Place>()
     fun addPlace(key: String, place: Place) {
-        require( key !in places ) {"Place with key $key is already in use in transition $name"}
-        places.set(key, place)
+        require(key !in places) { "Place with key $key is already in use in transition $name" }
+        places[key] = place
     }
 
     fun getPlace(key: String): Place {
         require(key in places.keys) { "Place ($key) not found in transition $name" }
         return places.getValue(key)
     }
+
+    fun getPlacesStartingWith(keyPrefix: String): List<Place> = places
+        .filter { (k, _) -> k.startsWith(keyPrefix) }
+        .map { it.value }
+
 
     val fireableTokenId: List<UUID>? get() = canFirePredicate(this)
 }
