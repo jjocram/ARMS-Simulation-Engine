17773b7e47d497488adbd445425e32b96f042559
Marco Ferrati <marco.ferrati@gmail.com>
2024-12-02 15:16:03 +0100

    feat: Add writing metrics to JSON file

--- src/main/kotlin/Main.kt
+++ src/main/kotlin/Main.kt
@@ -1,7 +1,16 @@
 import kotlinx.datetime.Instant
+import org.json.JSONException
+import org.json.JSONObject
 import org.kalasim.*
 import java.io.File
+import java.io.FileWriter
+import java.io.IOException
+import java.io.PrintWriter
 import kotlin.system.exitProcess
+
+fun Environment.totalTime(): Long {
+    return (now - startDate).inWholeSeconds / 60
+}
 
 fun main(args: Array<String>) {
     val processFile = File(args[0])
@@ -19,7 +28,35 @@ fun main(args: Array<String>) {
         object : Component("Watcher") {
             override fun repeatedProcess(): Sequence<Component> = sequence {
                 if (process.places.getValue("end").count() == process.totalProductRequest && process.places.getValue("end_product").count() == process.totalProductRequest) {
-                    println("There are 100 token in the last place")
+                    println("There are ${process.places.getValue("end").count()} tokens in the last place")
+
+                    val json = JSONObject()
+
+                    try {
+                        json.put("simulation", mapOf<String, Any>(
+                            "totalTime" to env.totalTime()
+                        ))
+
+                        json.put("executors", process.executors.map { (_, executor) ->
+                            mapOf<String, Any>(
+                                "id" to executor.id,
+                                "maxWaitTimeInQueue" to executor.jobsInQueueMetrics.max,
+                                "busy" to executor.totalBusyTime,
+                                "idle" to executor.totalIdleTime
+                            )
+                        })
+                    } catch (e: JSONException) {
+                        e.printStackTrace()
+                    }
+
+
+                    try {
+                        PrintWriter(FileWriter("example.json")).use { it.write(json.toString()) }
+                    } catch (e: IOException) {
+                        e.printStackTrace()
+                    }
+
+
                     process.executors.forEach { _, executor ->
                         println("""Id: ${executor.id} (${executor.name})
                             |   maxWaitTimeInQueue: ${executor.jobsInQueueMetrics.max}
