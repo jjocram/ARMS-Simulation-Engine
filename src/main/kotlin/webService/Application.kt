e707e7a109245ecadbff77e6790c37db9d7cbc6b
Marco Ferrati <marco.ferrati@gmail.com>
2025-07-15 21:55:02 +0200

    chore: add log with version number

--- src/main/kotlin/webService/Application.kt
+++ src/main/kotlin/webService/Application.kt
@@ -1,12 +1,14 @@
 package webService
 
 import io.ktor.server.application.Application
+import io.ktor.server.application.log
 
 fun main(args: Array<String>) {
     io.ktor.server.netty.EngineMain.main(args)
 }
 
 fun Application.module() {
+    log.info("ARMS version: 1.1.a")
     configureSerialization()
     configureRouting()
 }
